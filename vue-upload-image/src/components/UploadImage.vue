38bdf8b4a468da8fdf671ae4a1a822d895be1545
eddiecalm <30434361+eddiecalm@users.noreply.github.com>
2019-11-13 16:08:19 +0000

    css added

--- vue-upload-image/src/components/UploadImage.vue
+++ vue-upload-image/src/components/UploadImage.vue
@@ -2,11 +2,12 @@
     <div id="upload-image">
         <div v-if="!image">
             <h2>Select an image</h2>
-            <input type="file" @change="onFileChange($event)">
+            <input type="file" @change="onFileChange($event)" id="upload-image-input-id" hidden>
+            <label for="upload-image-input-id"  class="upload-img-btn">Upload</label>
         </div>
         <div v-else>
-            <img :src="image" />
-            <button @click="removeImage(image)">Remove image</button>
+            <img :src="image" class="img"/>
+            <button @click="removeImage(image)" >Remove image</button>
         </div>
     </div>
 </template>
@@ -38,4 +39,18 @@ export default {
 </script>
 
 <style scoped>
+.upload-img-btn {
+    background-color: #95C6A4;
+    color: #358D4F;
+    text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
+    cursor: pointer;
+    width: 150px;
+    text-align: center;
+    margin: auto;
+    border-radius: 5px;
+}
+
+.img {
+    width: 50%;
+}
 </style>
